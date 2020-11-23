9dbf8e548f24fb10ebe9ea376b39c37536098ab6
OlekGalgan <olekgalgan@gmail.com>
2020-11-23 20:12:42 +0100

    Dodano klase User

---
+++ BibliotekaKlas/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliotekaKlas
+{
+    class User
+    {
+        private string login;
+        private string password;
+
+        public User(string login, string password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
